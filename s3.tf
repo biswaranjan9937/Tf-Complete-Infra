db7508152ac7cfe3d39a73803c74d56854096243
biswaranjan <biswaranjan.b@cloudworkmates.com>
2026-01-15 17:21:31 +0530

    fix: update s3 bucket policy

--- s3.tf
+++ s3.tf
@@ -43,22 +43,23 @@ resource "aws_s3_bucket_policy" "alb_log_policy" {
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
-        },
-        "Action": "s3:PutObject",
-        "Resource": "${aws_s3_bucket.alb_log_s3.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-      },
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
-        },
-        "Action": "s3:GetBucketAcl",
-        "Resource": "${aws_s3_bucket.alb_log_s3.arn}"
-      }
+        {
+            "Sid": "ALBAccessLogsWrite",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
+            },
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectAcl"
+            ],
+            "Resource": "${aws_s3_bucket.alb_log_s3.arn}/*AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
     ]
   })
 }
