69ad3e241ed73c5da3b24b9424afc3cdacd3c3e2
official-manishs <manish.s@cloudworkmates.com>
2026-01-16 15:39:26 +0530

    adding patch window

--- patch_manager.tf
+++ patch_manager.tf
@@ -89,89 +89,89 @@ resource "aws_ssm_default_patch_baseline" "ubuntu_linux_default" {
 #   }
 # }
 
-# Patch Group - Associates baseline with tagged instances
-# resource "aws_ssm_patch_group" "patch_group" {
-#   baseline_id = aws_ssm_patch_baseline.linux_baseline.id
-#   patch_group = "production-servers" # Match this with EC2 instance tag
-# }
+#Patch Group - Associates baseline with tagged instances
+resource "aws_ssm_patch_group" "patch_group" {
+  baseline_id = aws_ssm_patch_baseline.alma_linux_baseline.id
+  patch_group = "production-servers" # Match this with EC2 instance tag
+}
 
-# Maintenance Window - Schedule for patching
-# resource "aws_ssm_maintenance_window" "patching_window" {
-#   name              = "patching-maintenance-window"
-#   description       = "Maintenance window for automated patching"
-#   schedule          = "cron(0 2 ? * SUN *)" # Every Sunday at 2 AM UTC
-#   duration          = 3                      # 3 hours
-#   cutoff            = 1                      # Stop 1 hour before end
-#   allow_unassociated_targets = false
+#Maintenance Window - Schedule for patching
+resource "aws_ssm_maintenance_window" "patching_window" {
+  name              = "patching-maintenance-window"
+  description       = "Maintenance window for automated patching"
+  schedule          = "cron(15 10 16 1 ? 2026)"  # Jan 16, 2026 at 10:15 UTC (15:45 IST)
+  duration          = 3                      # 3 hours
+  cutoff            = 1                      # Stop 1 hour before end
+  allow_unassociated_targets = false
 
-#   tags = {
-#     Name        = "patching-maintenance-window"
-#     Environment = var.environment
-#   }
-# }
+  tags = {
+    Name        = "patching-maintenance-window"
+    Environment = var.environment
+  }
+}
 
-# Maintenance Window Target - Which instances to patch
-# resource "aws_ssm_maintenance_window_target" "patching_target" {
-#   window_id     = aws_ssm_maintenance_window.patching_window.id
-#   name          = "patching-targets"
-#   description   = "Instances to be patched"
-#   resource_type = "INSTANCE"
+#Maintenance Window Target - Which instances to patch
+resource "aws_ssm_maintenance_window_target" "patching_target" {
+  window_id     = aws_ssm_maintenance_window.patching_window.id
+  name          = "patching-targets"
+  description   = "Instances to be patched"
+  resource_type = "INSTANCE"
 
-#   targets {
-#     key    = "tag:PatchGroup"
-#     values = ["production-servers"] # EC2 instances must have this tag
-#   }
-# }
+  targets {
+    key    = "tag:wm-AutoStartStop"
+    values = ["yes"] # EC2 instances must have this tag
+  }
+}
 
-# Maintenance Window Task - Execute patching
-# resource "aws_ssm_maintenance_window_task" "patch_task" {
-#   window_id        = aws_ssm_maintenance_window.patching_window.id
-#   task_type        = "RUN_COMMAND"
-#   task_arn         = "AWS-RunPatchBaseline"
-#   priority         = 1
-#   service_role_arn = aws_iam_role.ssm_maintenance_window_role.arn
-#   max_concurrency  = "50%"  # Patch 50% of instances at a time
-#   max_errors       = "25%"  # Stop if 25% fail
+#Maintenance Window Task - Execute patching
+resource "aws_ssm_maintenance_window_task" "patch_task" {
+  window_id        = aws_ssm_maintenance_window.patching_window.id
+  task_type        = "RUN_COMMAND"
+  task_arn         = "AWS-RunPatchBaseline"
+  priority         = 1
+  service_role_arn = aws_iam_role.ssm_maintenance_window_role.arn
+  max_concurrency  = "50%"  # Patch 50% of instances at a time
+  max_errors       = "25%"  # Stop if 25% fail
 
-#   targets {
-#     key    = "WindowTargetIds"
-#     values = [aws_ssm_maintenance_window_target.patching_target.id]
-#   }
+  targets {
+    key    = "WindowTargetIds"
+    values = [aws_ssm_maintenance_window_target.patching_target.id]
+  }
 
-#   task_invocation_parameters {
-#     run_command_parameters {
-#       parameter {
-#         name   = "Operation"
-#         values = ["Install"]
-#       }
-#       parameter {
-#         name   = "RebootOption"
-#         values = ["RebootIfNeeded"] # Options: RebootIfNeeded, NoReboot
-#       }
-#     }
-#   }
-# }
+  task_invocation_parameters {
+    run_command_parameters {
+      parameter {
+        name   = "Operation"
+        values = ["Install"]
+      }
+      parameter {
+        name   = "RebootOption"
+        values = ["RebootIfNeeded"] # Options: RebootIfNeeded, NoReboot
+      }
+    }
+  }
+}
 
 # IAM Role for Maintenance Window
-# resource "aws_iam_role" "ssm_maintenance_window_role" {
-#   name = "ssm-maintenance-window-role"
+resource "aws_iam_role" "ssm_maintenance_window_role" {
+  name = "ssm-maintenance-window-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ssm.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ssm.amazonaws.com"
+      }
+    }]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "ssm_maintenance_window_policy" {
-#   role       = aws_iam_role.ssm_maintenance_window_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMMaintenanceWindowRole"
-# }
+resource "aws_iam_role_policy_attachment" "ssm_maintenance_window_policy" {
+  role       = aws_iam_role.ssm_maintenance_window_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMMaintenanceWindowRole"
+}
 
 # # SNS Topic for Patch Notifications (Optional)
 # resource "aws_sns_topic" "patch_notifications" {
