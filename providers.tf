f8a07b3a3efae3d1b276ec0e40881fcbfd83fe5a
biswaranjan <biswaranjan.b@cloudworkmates.com>
2026-03-02 17:30:33 +0530

    update provider block

--- providers.tf
+++ providers.tf
@@ -42,17 +42,17 @@ provider "kubectl" {
   #load_config_file = false
 }
 
-# provider "helm" {
-#   kubernetes  {
-#     host                   = module.eks_cluster.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
-#     exec  {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-#       command     = "aws"
-#     }
-#   }
-# }
+provider "helm" {
+  kubernetes  {
+    host                   = module.eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+    exec  {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
+}
 
 # provider "kubernetes" {
 #   host                   = module.eks_cluster.cluster_endpoint
@@ -64,7 +64,7 @@ provider "kubectl" {
 #   }
 # }
 
-provider "helm" {
-}
+
+
 
 ################################
